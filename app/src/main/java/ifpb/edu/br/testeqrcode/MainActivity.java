a8cbdef8c37a7a4d4f37cd909832a65ca66fbaa1
Bruno Lira <brunolyr4@gmail.com>
2016-03-10 20:21:09 -0300

    update

--- app/src/main/java/ifpb/edu/br/testeqrcode/MainActivity.java
+++ app/src/main/java/ifpb/edu/br/testeqrcode/MainActivity.java
@@ -1,9 +1,10 @@
 package ifpb.edu.br.testeqrcode;
 
+import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -11,16 +12,16 @@ import android.widget.Toast;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
+
+    //Botões da activity_main
+    private Button scanBtn;
+    private TextView formatTxt, contentTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Botões da activity_main
-        Button scanBtn;
-        TextView formatTxt, contentTxt;
 
         //Atribuindo valores ao button e ao textview
         scanBtn = (Button) findViewById(R.id.scan_button);
@@ -28,31 +29,24 @@ public class MainActivity extends Activity {
         contentTxt = (TextView) findViewById(R.id.scan_content);
 
         //Adicionando lista ao button
-        //scanBtn.setOnClickListener(this);
-        scanBtn.setOnClickListener(new View.OnClickListener() {
-            //Método para resposta dos click
-           @Override
-            public void onClick(View v) {
-                if (v.getId() == R.id.scan_button) {
-                    IntentIntegrator scanIntegrator = new IntentIntegrator(MainActivity.this);
-                    scanIntegrator.initiateScan();
-                }
-            }
-        });
+        scanBtn.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        if (v.getId() == R.id.scan_button) {
+            IntentIntegrator scanIntegrator = new IntentIntegrator(MainActivity.this);
+            scanIntegrator.initiateScan();
+        }
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        //retrieve scan result
         IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
 
         if (scanningResult != null) {
-            //we have a result
             String scanContent = scanningResult.getContents();
             String scanFormat = scanningResult.getFormatName();
 
-            TextView formatTxt = null;
             formatTxt.setText("FORMAT: " + scanFormat);
-            TextView contentTxt = null;
             contentTxt.setText("CONTENT: " + scanContent);
 
         } else {
@@ -61,11 +55,6 @@ public class MainActivity extends Activity {
             toast.show();
         }
     }
-
-   // @Override
-    //public void onClick(View v) {
-
-    //}
 }
 
 
